a99374c959250d79960c43b5b979af49cde087e5
jongwon <growjong8802@gmail.com>
2025-09-08 23:17:40 +0900

    build : 빌드 파일 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/be08-3rd-PotatoSpot-DAZZLE/", // 예: '/my-vue-project/'
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
